5dd7e506849941ad749db362a7724799e2acfb94
Aryan Gandhi <aryandgandhi@gmail.com>
2024-01-25 17:50:14 -0600

    mp1

--- CS498MP1/instance2/app.py
+++ CS498MP1/instance2/app.py
@@ -1,1 +1,24 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+seed_value = 0
+
+@app.route('/', methods=['GET'])
+def get_seed():
+
+    return jsonify(seed=seed_value)
+
+@app.route('/', methods=['POST'])
+def update_seed():
+    global seed_value
+
+    request_data = request.get_json()
+    seed_value = request_data.get('num', seed_value)
+    
+    return jsonify(message="Seed value updated", new_seed=seed_value), 200
+
+if __name__ == '__main__':
+
+    app.run(host='0.0.0.0', port=8080)
